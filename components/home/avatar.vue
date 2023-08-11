991c2124be8fe54ebfbac38ae386fa41914453c0
Sami <saxgetty4@gmail.com>
2023-08-11 15:49:22 -0700

    Hotfix avatar from being slightly left

--- components/home/avatar.vue
+++ components/home/avatar.vue
@@ -1,3 +1,3 @@
 <template>
-    <img src="https://i.imgur.com/DpyYTgo.png" class="align-middle border-none -m-16 -ml-20 lg:-ml-16 max-w-[350px]"/>
+    <img src="https://i.imgur.com/DpyYTgo.png" class="align-middle border-none -m-16 lg:-ml-16 max-w-[350px]"/>
 </template>
