2edae507d99b6394d7e10e321a3fec6525edef22
Sami <saxgetty4@gmail.com>
2023-08-18 15:43:59 -0700

    Added Pinterest HTML tag to header

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,8 @@
   <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
   <meta name="msapplication-TileColor" content="#da532c" />
   <meta name="theme-color" content="#ffffff" />
-
+  <meta name="p:domain_verify" content="d00a4330ea1e6735fb7debdafca1670c"/> <!-- Pinterest HTML tag -->
+  
   <Disclosure
     as="nav"
     class="z-20 bg-cool-green shadow sticky top-0"
