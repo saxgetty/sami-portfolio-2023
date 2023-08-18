75834253c46f40b17cbcd25c7e0f49f1aa83ceac
Sami <saxgetty4@gmail.com>
2023-08-18 16:08:51 -0700

    Trying file upload for HTML tag test

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  head: {
-    meta: [
-      {
-        name: 'p:domain_verify',
-        content: 'd00a4330ea1e6735fb7debdafca1670c' // Pinterest HTML tag
-      }
-    ],
-  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
