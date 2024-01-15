f27227d20c1711c67c06d58272216f00b0e9bbda
Sami <saxgetty4@gmail.com>
2024-01-14 20:03:30 -0800

    Beginning start to revamp

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
+  },
+  layouts: {
+    default: "~/layouts/default.vue",
   },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,77 @@
 <template>
-  <div>
-    <div
-      class="flex flex-col items-center p-4 pt-20 pb-20 md-pad:p-20 lg:min-h-screen animate-slide-in"
-    >
-      <div
-        class="max-w-6xl break-words bg-white w-full mb-6 shadow-2xl rounded-md"
-      >
-        <div class="px-6">
-          <div class="flex flex-wrap justify-center">
-            <div
-              class="w-full flex justify-center mb-5 animate-slide-in-delayed"
-            >
-              <HomeAvatar />
-            </div>
-          </div>
-          <div class="text-center mt-20 animate-slide-in-delayed">
-            <h3
-              class="text-7xl font-shrikhand text-cool-purple drop-shadow-lg leading-none mb-1"
-            >
-              Sami Saxton-Getty
-            </h3>
-            <div
-              class="text-2xl mt-5 mb-2 text-cool-green-dark font-bold uppercase"
-            >
-              <i class="fas fa-map-marker-alt mr-2 opacity-75"></i>Temecula,
-              California
-            </div>
-          </div>
-          <div class="text-center">
-            <div class="flex flex-wrap justify-center">
-              <div
-                class="mt-10 pb-20 mx-auto p-4 w-full items-center sm:w-auto"
-              >
-                <div class="flex flex-col sm:flex-row">
-                  <div class="animate-slide-in-delayed">
-                    <HomeSocialsEmail />
-                  </div>
-                  <div
-                    class="py-10 px-0 sm:py-0 sm:px-10 animate-slide-in-delayed"
-                  >
-                    <HomeSocialsLinkedin />
-                  </div>
-                  <div
-                    class="pb-10 pr-0 sm:pb-0 sm:pr-10 animate-slide-in-delayed"
-                  >
-                    <HomeSocialsGithub />
-                  </div>
-                  <div class="animate-slide-in-delayed">
-                    <HomeSocialsEtsy />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="flex flex-col">
+    <section id="one" class="panel-1 flex"><h1>ONE</h1></section>
+    <section id="two" class="panel-2 flex"><h1>TWO</h1></section>
+    <section id="three" class="panel-3 flex"><h1>THREE</h1></section>
+    <section id="four" class="panel-4 flex"><h1>FOUR</h1></section>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue"
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+gsap.registerPlugin(ScrollTrigger)
+
+// Receive the navigation links from the layout
+const navLinks = ref([])
+
+// Use navLinks in your logic
+onMounted(() => {
+
+  let panelCreated = false;
+  let panelNum = 4;
+
+  const createPanel = (index) => {
+      const section = document.createElement("section");
+      const h1 = document.createElement("h1");
+      container.appendChild(section);
+      section.appendChild(h1);
+      h1.innerHTML = `Panel ${index}`;
+      gsap.set(section, {
+          backgroundColor: colors[gsap.utils.random(0, colors.length-1, 1)],
+          className: `panel-${index}`
+      }); 
+  };
+
+  ScrollTrigger.create({
+      trigger: document.body,
+      start: "top top",
+      end: "bottom bottom",
+      onUpdate: (self) => {
+          let progress = self.progress.toFixed(2);
+          if (progress >= 0.9 && self.direction === 1) {
+              createPanel(panelNum++);
+              ScrollTrigger.refresh();
+          }
+      },
+  });
+
+  gsap.utils.toArray(".panel").forEach((panel, i) => {
+    let trigger = ScrollTrigger.create({
+      trigger: panel,
+      start: "top top",
+      pin: true,
+      pinSpacing: false,
+    })
+
+    let nav = navLinks.value[i]
+
+    if (nav) {
+      nav.addEventListener("click", function (e) {
+        e.preventDefault()
+        gsap.to(window, {
+          duration: 1,
+          scrollTo: trigger.start,
+        })
+      })
+    } else {
+      console.error("nav is undefined for index", i)
+    }
+  })
+})
+</script>
 
 <style>
 @import "../assets/css/main.css";
